25f1a11b76d5ad29f96f388e9fc35375b4347c15
HrachyaJ <152434721+HrachyaJ@users.noreply.github.com>
2024-07-07 16:16:01 +0300

    Add files via upload

--- javascript/todo-list-js.js
+++ javascript/todo-list-js.js
@@ -1,42 +1,57 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+  name: 'make money',
+  dueDate: '2024-07-05'
+}, {
+  name: 'buy a car',
+  dueDate: '2024-07-05'
+}];
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const name = inputElement.value;
+renderTodoList();
 
-  todoList.push(name);
-  console.log(todoList);
-
-  inputElement.value = '';
-  // document.querySelector('.todoDisplay').innerHTML = name;
-}
-
-renderToDoList();
-
-function renderToDoList() {
+function renderTodoList() {
   let todoListHTML = '';
 
-  for (var i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div class="todo-item">${name}</div>
+      <div class="todo-item">${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-button">Delete</button>
+    `;
     todoListHTML += html;
   }
 
-  console.log(todoListHTML);
-
-  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-
-  renderToDoList();
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
 }
-/*
-const todoList2 = [
-  'make dinner',
-  'wash dishes',
-  'watch youtube'
-];
 
-for (var i = 0; i < todoList2.length; i++) {
-  const value = todoList2[i];
-  console.log(value);
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value.trim();
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  if (name !== '') {
+    todoList.push({
+      name,
+      dueDate
+    });
+
+    inputElement.value = '';
+
+    renderTodoList();
+  }
 }
-*/
+
+// Add event listener for the 'keydown' event on the name input field
+document.querySelector('.js-name-input').addEventListener('keydown', function(event) {
+  // Check if the pressed key is the Enter key
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+});
